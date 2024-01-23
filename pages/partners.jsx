e5c54080882e82da8a1b280d1acbbb5f349ec86f
nsanctus <132467914+nsanctus@users.noreply.github.com>
2024-01-23 17:55:24 +0300

    partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,7 +16,7 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me">
+            <a href="https://www.twitch.tv/vuupee">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1147890782494281830/1199120086544416859/Vuupee_Yuvarlak.png?ex=65c1629b&is=65aeed9b&hm=7ff094a4393681f87229afd16fb802539a0eb2f58d52d74e8b0d36bc2b9fd08d&" />
                             <div>
@@ -33,7 +33,7 @@ export default function Partners() {
 
 
                             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me/partners/64db583a4292ca1066ad0a1b">
+            <a href="https://www.instagram.com/talhakya9?igsh=MzNlNGNkZWQ4Mg==">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1147890782494281830/1199120086544416859/Vuupee_Yuvarlak.png?ex=65c1629b&is=65aeed9b&hm=7ff094a4393681f87229afd16fb802539a0eb2f58d52d74e8b0d36bc2b9fd08d&" />
                             <div>
@@ -41,6 +41,7 @@ export default function Partners() {
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
                                    Ns'Sanctus - Talha Kaya
+                                        İnstagram Profili İçin Üzerime Tıklayabilirsin :)
                                     </p>
                                 </div>     
                             </div>
@@ -50,14 +51,15 @@ export default function Partners() {
 
                 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me">
+            <a href="https://www.twitch.tv/redhookgaming">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1147890782494281830/1199120086544416859/Vuupee_Yuvarlak.png?ex=65c1629b&is=65aeed9b&hm=7ff094a4393681f87229afd16fb802539a0eb2f58d52d74e8b0d36bc2b9fd08d&" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1199337906452578344/1199365859777458216/rh_logo_kopya.png?ex=65c24780&is=65afd280&hm=dd2d4b21144c0fc9a625dae811ba0ef4abe5c293300217b0f05b48a475fa7163&" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">RedhhokGaming</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Yönetim</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
                                      RedhookGaming - Oğulcan
+                                        Twicth Profili İçin Üzerime Tıklayabilirsin :)
                                     </p>
                                 </div>     
                             </div>
