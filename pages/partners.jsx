51ba0d38f3eee4f1a41ff8ffd777686677396f0f
nsanctus <132467914+nsanctus@users.noreply.github.com>
2024-01-23 04:19:49 +0300

    partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,12 +18,12 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://codeshare.me">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1147890782494281830/1199122123839512679/Redhookgaming.png?ex=65c16480&is=65aeef80&hm=aa884457b2ea8ee4421c7e6707028f37f0d2e8274a57ae66726b96ff0d497b03&" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1147890782494281830/1199120086544416859/Vuupee_Yuvarlak.png?ex=65c1629b&is=65aeed9b&hm=7ff094a4393681f87229afd16fb802539a0eb2f58d52d74e8b0d36bc2b9fd08d&" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">RedhookGaming</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Founder</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                     Redhood .
+                                     Vuupee - Cihan .
                                     </p>
                                 </div>     
                             </div>
@@ -40,7 +40,7 @@ export default function Partners() {
                                 <h1 className="leading-none text-3xl font-bold text-white">Developer - Geliştirici</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                   NsSanctus - Talha Kaya
+                                   Ns'Sanctus - Talha Kaya
                                     </p>
                                 </div>     
                             </div>
